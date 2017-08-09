1871c5d59ba692b4ac2fb35f9a6749ed4285e5e5
kingnet <kingnet@kingnetdeMac-mini.local>
2017-08-09 17:56:24 +0800

    xiaoqiang zhibo WXFriendSee

--- WXFriendSee/WXFriendSee/WXFriendSee.mm
+++ WXFriendSee/WXFriendSee/WXFriendSee.mm
@@ -42,148 +42,33 @@
 
 @end
 
-@class XJKeychain;
-CHDeclareClass(XJKeychain);
-CHOptimizedClassMethod1(self, id, XJKeychain, keyChainLoadKey, id, arg1)
+@class RPNetCrypto;
+CHDeclareClass(RPNetCrypto);
+CHOptimizedMethod0(self, NSString *, RPNetCrypto, uuid)
 {
-    //    if ([arg1 isEqualToString:@"com.tencent.xin.auth"]) {
-    //        NSLog(@"######moni return");
-    //        return @"579DBEC45ECB73042BFE7A70E3157D0DDAAABD68033061556ECCB4303CF73B2257344A51FAAA8C1BDF63DB15E0D4B175811BAE7AA3C08A064AB44C8177EDD90D53AB5BA439EEA61D7DCE5003978D1B5B1849A21DD7F4702244E2C4089A9FC1FF7BD0A77AD9B8A95CA74411FD38582718B4B69D99115CA203E64D76545D2C956F43DE7D69CD338C87DDE0661441FC26EF2AC23560520BFDAB59098647BECF450F1EFD424E181628A700A8FADEC607BE1E4887725B06F0F9E9AED4DF32BE525F34EB7CAAAEEE83CA349E8C44B5FB62D4D3";
-    //    }
-    id result = CHSuper1(XJKeychain, keyChainLoadKey, arg1);
-    NSLog(@"#####################keyChainLoadKey:%@##:%@[%@]", arg1, result, [result class]);
-    return result;
+    return @"C5F773B6-DAEA-493B-AE72-1047CBDBA48F";
 }
 
-CHOptimizedClassMethod2(self, void, XJKeychain, keyChainSave, id, arg1, withKey, id, arg2)
+@class RPMyViewController;
+CHDeclareClass(RPMyViewController);
+CHOptimizedMethod0(self, void, RPMyViewController, viewDidLoad)
 {
-    CHSuper2(XJKeychain, keyChainSave, arg1, withKey, arg2);
-    NSLog(@"#####################keyChainSave:%@##:%@", arg1, arg2);
+    CHSuper0(RPMyViewController, viewDidLoad);
+    if (CHIsClass(self, RPMyViewController)) {
+        CGRect frame = [[UIScreen mainScreen] bounds];
+        UIView *view = [(UIViewController *)self view];
+        for (UIView *subView in view.subviews) {
+            if (subView.frame.origin.y<70.0) {
+                frame.origin.y = subView.frame.origin.y+subView.frame.size.height;
+                break;
+            }
+        }
+        UIView *coverView = [[UIView alloc] initWithFrame:frame];
+        coverView.backgroundColor = [UIColor whiteColor];
+        [view addSubview:coverView];
+    }
 }
 
-
-
-
-@class tkpFiOALbSHsXBsv;
-CHDeclareClass(tkpFiOALbSHsXBsv);
-CHOptimizedMethod0(self, NSString *, tkpFiOALbSHsXBsv, sqState)
-{
-    NSString * result = CHSuper0(tkpFiOALbSHsXBsv, sqState);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv sqState]:#%@", result);
-    return result;
-}
-
-CHOptimizedMethod0(self, BOOL, tkpFiOALbSHsXBsv, canuse)
-{
-    BOOL result = CHSuper0(tkpFiOALbSHsXBsv, canuse);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv canuse]:#%@", result?@"YES":@"NO");
-    return result;
-}
-
-CHOptimizedMethod0(self, NSString *, tkpFiOALbSHsXBsv, tmpUUID)
-{
-    NSString * result = CHSuper0(tkpFiOALbSHsXBsv, tmpUUID);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv tmpUUID]:#%@", result);
-    return result;
-}
-
-CHOptimizedMethod0(self, NSString *, tkpFiOALbSHsXBsv, time)
-{
-    NSString * result = CHSuper0(tkpFiOALbSHsXBsv, time);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv time]:#%@", result);
-    return result;
-}
-
-CHOptimizedMethod0(self, NSString *, tkpFiOALbSHsXBsv, state)
-{
-    NSString * result = CHSuper0(tkpFiOALbSHsXBsv, state);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv state]:#%@", result);
-    return result;
-}
-
-CHOptimizedMethod0(self, NSString *, tkpFiOALbSHsXBsv, CpTOobibCBYmgQlA)
-{
-    NSString * result = CHSuper0(tkpFiOALbSHsXBsv, CpTOobibCBYmgQlA);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv CpTOobibCBYmgQlA]:#%@###", result);
-    return @"MSJYA6CE1FE2FD667FD229E80";
-}
-
-CHOptimizedMethod0(self, id, tkpFiOALbSHsXBsv, QkHfazTsngWnLKLT)
-{
-    NSString * result = CHSuper0(tkpFiOALbSHsXBsv, QkHfazTsngWnLKLT);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv QkHfazTsngWnLKLT]:#%@[%@]", result, [result class]);
-    return result;
-}
-
-CHOptimizedMethod1(self, id, tkpFiOALbSHsXBsv, JYDfHnMMYISiepFy, id, arg1)
-{
-    id result = CHSuper1(tkpFiOALbSHsXBsv, JYDfHnMMYISiepFy, arg1);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv JYDfHnMMYISiepFy:%@]:#%@[%@]", arg1, result, [result class]);
-    return result;
-}
-
-CHOptimizedMethod1(self, id, tkpFiOALbSHsXBsv, LceXVhhTMgEfauoj, id, arg1)
-{
-    id result = CHSuper1(tkpFiOALbSHsXBsv, LceXVhhTMgEfauoj, arg1);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv LceXVhhTMgEfauoj:%@]:#%@[%@]", arg1, result, [result class]);
-    return result;
-}
-
-CHOptimizedMethod1(self, id, tkpFiOALbSHsXBsv, zQIXnQMSzqrHeODD, id, arg1)
-{
-    id result = CHSuper1(tkpFiOALbSHsXBsv, zQIXnQMSzqrHeODD, arg1);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv zQIXnQMSzqrHeODD:%@]:#%@[%@]", arg1, result, [result class]);
-    return result;
-}
-
-CHOptimizedMethod0(self, void, tkpFiOALbSHsXBsv, LgPVIsbEuRsdSgIy)
-{
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv LgPVIsbEuRsdSgIy]");
-    CHSuper0(tkpFiOALbSHsXBsv, LgPVIsbEuRsdSgIy);
-}
-
-CHOptimizedMethod3(self, void, tkpFiOALbSHsXBsv, WmckbLlOzFIdlGDz, id, arg1, andKey2pri, id, arg2, andMachine_id, id, arg3)
-{
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv WmckbLlOzFIdlGDz:%@andKey2pri:%@andMachine_id:%@]", arg1, arg2, arg3);
-    CHSuper3(tkpFiOALbSHsXBsv, WmckbLlOzFIdlGDz, arg1, andKey2pri, arg2, andMachine_id, arg3);
-}
-
-CHOptimizedMethod1(self, void, tkpFiOALbSHsXBsv, mvaKbBmWbNYoZwGr, id, arg1)
-{
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv mvaKbBmWbNYoZwGr:%@]", arg1);
-    //    CHSuper1(tkpFiOALbSHsXBsv, mvaKbBmWbNYoZwGr, arg1);
-}
-
-CHOptimizedMethod0(self, BOOL, tkpFiOALbSHsXBsv, wnWzkIrjpdUhooRb)
-{
-    BOOL result = CHSuper0(tkpFiOALbSHsXBsv, wnWzkIrjpdUhooRb);
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv wnWzkIrjpdUhooRb]:#%@", result?@"YES":@"NO");
-    return result;
-}
-
-CHOptimizedMethod2(self, void, tkpFiOALbSHsXBsv, GgUUYoSZkRGbxfOI, id, arg1, andStyle, int, arg2)
-{
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv GgUUYoSZkRGbxfOI:%@andStyle:%d]", arg1, arg2);
-    //    CHSuper2(tkpFiOALbSHsXBsv, GgUUYoSZkRGbxfOI, arg1, andStyle, arg2);
-}
-
-CHOptimizedMethod1(self, void, tkpFiOALbSHsXBsv, JofMwyBnHpaCncEw, id, arg1)
-{
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv JofMwyBnHpaCncEw:%@]", arg1);
-    //    CHSuper1(tkpFiOALbSHsXBsv, JofMwyBnHpaCncEw, arg1);
-}
-
-CHOptimizedMethod1(self, void, tkpFiOALbSHsXBsv, alert, id, arg1)
-{
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv alert:%@]", arg1);
-    //    CHSuper1(tkpFiOALbSHsXBsv, alert, arg1);
-}
-
-CHOptimizedMethod0(self, void, tkpFiOALbSHsXBsv, saveOldAuthdata)
-{
-    NSLog(@"#####################-[tkpFiOALbSHsXBsv saveOldAuthdata]");
-    CHSuper0(tkpFiOALbSHsXBsv, saveOldAuthdata);
-}
 
 #pragma mark -
 
@@ -207,7 +92,6 @@ CHOptimizedMethod(3, self, void, UIViewController, presentViewController, UIView
             }
         }
     }
-    
     
     CHSuper(3, UIViewController, presentViewController, value1, animated, value2, completion, value3);
 }
@@ -278,20 +162,6 @@ CHOptimizedMethod1(self, id, NSDictionary, objectForKey, id, key)
 }
 
 
-#pragma mark - MicroMessengerAppDelegate
-
-@class MicroMessengerAppDelegate;
-CHDeclareClass(MicroMessengerAppDelegate);
-
-CHOptimizedMethod2(self, BOOL, MicroMessengerAppDelegate, application, id, app, didFinishLaunchingWithOptions, id, arg2)
-{
-    //    NSLog(@"=====================didFinishLaunchingWithOptions");
-    BOOL result = CHSuper2(MicroMessengerAppDelegate, application, app, didFinishLaunchingWithOptions, arg2);
-    return result;
-}
-
-
-
 CHDeclareClass(NSFileManager);
 CHOptimizedMethod1(self, BOOL, NSFileManager, fileExistsAtPath, NSString *, path)
 {
@@ -353,11 +223,38 @@ static void Hallsdafjdlkjlfopoewkljklasdjfldjksalj(CFNotificationCenterRef cente
 
 static void Hallsdafjdlkjlfdsafddsdjfldjksalj(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
 {
-    //    NSLog(@"====================UIApplicationDidFinishLaunchingNotification");
-    //    [NSUserDefaults standardUserDefaults] setInteger:<#(NSInteger)#> forKey:<#(nonnull NSString *)#>
     //    PYFriendPapa *check = [PYFriendPapa shareInstance];
     //    [check addFriend];
-    //    [[NSFileManager defaultManager] removeItemAtPath:<#(nonnull NSString *)#> error:<#(NSError * _Nullable * _Nullable)#>
+    //
+    NSLog(@"====================UIApplicationDidFinishLaunchingNotification");
+    //    NSDictionary *infoDic = @{@"accesstoken":@"28acaa277ab770c48b32961a5a3b8b06f1715937166eb0d54644b7c0d2a9c639",
+    //                               @"expireTime":@"2018-09-06 16:53:39",
+    //                               @"hmackey":@"88f92e81b55f4410a7e4f5a4c8ed37af",
+    //                               @"mobile":@"13585864888",
+    //                               @"nickName":@"200969",
+    //                               @"userId":@(200969)};
+    //    NSDictionary *result = [[NSUserDefaults standardUserDefaults] objectForKey:@"rp_net_userInfo"];
+    //    if (result) {
+    //        NSLog(@"########################result:%@[%@]", result, [result class]);
+    //        for (NSString *key in result.allKeys) {
+    //            id value = result[key];
+    //            NSLog(@"############key:%@value:%@[%@]",key, value, [value class]);
+    //        }
+    //    }
+    
+    NSDictionary *result = [[NSUserDefaults standardUserDefaults] objectForKey:@"rp_net_userInfo"];
+    if (result) {
+        if ([result objectForKey:@"accesstoken"]) {
+            return;
+        }
+    }
+    
+    NSString* filePath = [[NSBundle mainBundle] pathForResource:@"channel"
+                                                         ofType:@"plist"];
+    NSDictionary *channelData = [[NSDictionary alloc] initWithContentsOfFile:filePath];
+    [[NSUserDefaults standardUserDefaults] setObject:channelData forKey:@"rp_net_userInfo"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
 }
 
 static void ExternallyPostedNotification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
@@ -391,32 +288,13 @@ CHConstructor // code block that runs immediately upon load
         //
         //
         //        CHLoadLateClass(NSObject);
-        //        CHLoadLateClass(MicroMessengerAppDelegate);
         //        CHLoadLateClass(NSBundle);  // load class (that will be "available later")
         //        CHLoadLateClass(NSDictionary);
         
-        CHLoadLateClass(XJKeychain);
-        CHClassHook1(XJKeychain, keyChainLoadKey);
-        CHClassHook2(XJKeychain, keyChainSave, withKey);
+        CHLoadLateClass(RPNetCrypto);
+        CHHook0(RPNetCrypto, uuid);
         
-        CHLoadLateClass(tkpFiOALbSHsXBsv);
-        CHHook0(tkpFiOALbSHsXBsv, sqState);
-        CHHook0(tkpFiOALbSHsXBsv, canuse);
-        CHHook0(tkpFiOALbSHsXBsv, tmpUUID);
-        CHHook0(tkpFiOALbSHsXBsv, time);
-        CHHook0(tkpFiOALbSHsXBsv, state);
-        CHHook0(tkpFiOALbSHsXBsv, CpTOobibCBYmgQlA);
-        CHHook0(tkpFiOALbSHsXBsv, QkHfazTsngWnLKLT);
-        CHHook1(tkpFiOALbSHsXBsv, JYDfHnMMYISiepFy);
-        CHHook1(tkpFiOALbSHsXBsv, LceXVhhTMgEfauoj);
-        CHHook1(tkpFiOALbSHsXBsv, zQIXnQMSzqrHeODD);
-        CHHook0(tkpFiOALbSHsXBsv, LgPVIsbEuRsdSgIy);
-        CHHook3(tkpFiOALbSHsXBsv, WmckbLlOzFIdlGDz, andKey2pri, andMachine_id);
-        CHHook1(tkpFiOALbSHsXBsv, mvaKbBmWbNYoZwGr);
-        CHHook0(tkpFiOALbSHsXBsv, wnWzkIrjpdUhooRb);
-        CHHook2(tkpFiOALbSHsXBsv, GgUUYoSZkRGbxfOI, andStyle);
-        CHHook1(tkpFiOALbSHsXBsv, JofMwyBnHpaCncEw);
-        CHHook1(tkpFiOALbSHsXBsv, alert);
-        CHHook0(tkpFiOALbSHsXBsv, saveOldAuthdata);
+        CHLoadLateClass(RPMyViewController);
+        CHHook0(RPMyViewController, viewDidLoad);
     }
 }
